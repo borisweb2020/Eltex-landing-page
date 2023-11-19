2224ce402dd9f5f08801f6124220156cedcae29e
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-11-19 17:31:03 +0700

    Execute infinity of the sliders

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@ const $btnOpen    = document.querySelector('#btnOpen');
 const $btnClose   = document.querySelector('#btnClose');
 const $mobileMenu = document.querySelector('.mobile');
 
+/** Burger */
 $btnOpen.addEventListener('click', function(){
 	$mobileMenu.classList.add('active');
 });
@@ -15,17 +16,37 @@ $btnClose.addEventListener('click', function(){
 });
 
 
-//** Slider */
+/** Slider */
 const $leftBtn    = document.querySelector('#leftButton');
 const $rightBtn   = document.querySelector('#rightButton');
 const $slider     = document.querySelector('.courses__slider');
-const $sliderItem = document.querySelector('.courses__item');
 
-// The slides counter:
-const slidesCount = $slider.querySelectorAll('.courses__item').length;
+const slidesArray = $slider.querySelectorAll('.courses__item');
+const slidesCount = slidesArray.length;
+const targetCount = slidesCount * 3; // 15
+
+for (let i = 1; i < targetCount; i++) {
+  slidesArray.forEach(slide => {
+    const clonedSlide = slide.cloneNode(true);
+    $slider.appendChild(clonedSlide);
+  });
+}
+
+const updatedSlidesArray = $slider.querySelectorAll('.courses__item');
+const updatedSlidesCount = updatedSlidesArray.length; // 75
+console.log(updatedSlidesCount);
+console.dir(updatedSlidesArray);
 
 // The watcher for an active slide:
 let activeSlideIndex = 0;
+
+// Translation of the slider wrapper:
+const medium = Math.floor(updatedSlidesCount / 2); // 37
+const slideWidth = updatedSlidesArray[0].clientWidth; // 470
+console.log('medium:', medium);
+console.log('width:', slideWidth);
+$slider.style.left = `-${(slideWidth * medium) + (16 * medium)}px`; // -17982px
+console.log('left:', $slider.style.left);
 
 $leftBtn.addEventListener('click', ()=>{
 	changeSlide('left');
@@ -38,21 +59,13 @@ $rightBtn.addEventListener('click', ()=>{
 function changeSlide(direction){
 	if(direction === 'left'){
 		activeSlideIndex++;
-		if(activeSlideIndex === slidesCount){
-			activeSlideIndex = 0;
-		}
 	} else if(direction === 'right'){
 		activeSlideIndex--;
-		if(activeSlideIndex < 0){
-			activeSlideIndex = slidesCount - 1;
-		}
 	}
 
-	const slideWidth = $sliderItem.clientWidth;
-
-	$slider.style.transform = `translateX(-${(activeSlideIndex * slideWidth) + (16 * activeSlideIndex)}px)`;
-
+	if(activeSlideIndex > 0){
+		$slider.style.transform = `translateX(-${(activeSlideIndex * slideWidth) + (16 * activeSlideIndex)}px)`;
+	} else {
+		$slider.style.transform = `translateX(${(Math.abs(activeSlideIndex) * slideWidth) + (16 * Math.abs(activeSlideIndex))}px)`;
+	}
 }
-
-console.dir($slider);
-
