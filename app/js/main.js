cbf7eefd5e1e3898e0ec6a85fb7623a94b47fa81
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-11-24 19:45:42 +0700

    Finish slider translation

--- app/js/main.js
+++ app/js/main.js
@@ -35,7 +35,6 @@ for (let i = 1; i < targetCount; i++) {
 const updatedSlidesArray = $slider.querySelectorAll('.courses__item');
 const updatedSlidesCount = updatedSlidesArray.length; // 75
 const medium = Math.floor(updatedSlidesCount / 2);
-console.log('medium:', medium);
 
 
 // The watcher for an active slide:
@@ -64,10 +63,7 @@ function slideWidth(array){
 }
 
 function translateSlider(slideWidth){
-
-	console.log('width:', slideWidth);
-	$slider.style.left = `-${(slideWidth * medium) + (16 * medium)}px`; // -17982px
-	console.log('left:', $slider.style.left);
+	$slider.style.left = `-${(slideWidth * medium) + (15 * medium)}px`;
 }
 
 function changeSlide(direction, slideWidth){
